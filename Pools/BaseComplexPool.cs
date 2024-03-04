d8067d7297366f542658ed6b6c3cfde7eeb41a7f
Vladimir <vladimir.empty@gmail.com>
2024-03-04 12:32:28 +0300

    Add pools

---
+++ Pools/BaseComplexPool.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmptyCommon.Pools
+{
+    public abstract class BaseComplexPool<Key, Value>
+    {
+        private readonly Dictionary<Key, BasePool<Value>> PoolsTable = new();
+
+        public void AddPoolObject(Key key, Value value)
+        {
+            PoolsTable[key] = CreatePool(value);
+        }
+
+        public Value Spawn(Key key)
+        {
+            if (!PoolsTable.ContainsKey(key))
+                throw new NullReferenceException($"[{this.GetType().Name}] Не содержит пулл с ключем - {key}");
+
+            return PoolsTable[key].Spawn();
+        }
+
+        public void Despawn(Key key, Value value)
+        {
+            if (PoolsTable.TryGetValue(key, out var pool))
+            {
+                pool.Despawn(value);
+            }
+            else
+            {
+                DestoryObject(value);
+            }
+        }
+
+        protected abstract BasePool<Value> CreatePool(Value value);
+        protected abstract void DestoryObject(Value value);
+    }
+}
---
+++ Pools/BasePool.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace EmptyCommon.Pools
+{
+    public abstract class BasePool<T>
+    {
+        private readonly Queue<T> Pool = new Queue<T>();
+
+        public BasePool(T prototype)
+        {
+            Prototype = prototype;
+        }
+
+        protected T Prototype { get; }
+
+        public T Spawn()
+        {
+            var @object = (Pool.Count > 0)
+                                ? Pool.Dequeue()
+                                : Create();
+
+            OnSpawn(@object);
+
+            return @object;
+        }
+
+        public void Despawn(T @object)
+        {
+            OnDespawn(@object);
+            Pool.Enqueue(@object);
+        }
+
+        protected abstract T Create();
+        protected virtual void OnSpawn(T spawnedObject) { }
+        protected virtual void OnDespawn(T despawnedObject) { }
+    }
+}
