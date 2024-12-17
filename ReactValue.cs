6fd279f217a950ba19f68a764aeba1af5d212ff9
Vladimir <vladimir.empty@gmail.com>
2024-12-17 19:12:12 +0300

    react value

---
+++ ReactValue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace EmptyCommon
+{
+    public sealed class ReactValue<T>
+    {
+        private T _value;
+
+        public ReactValue(T value = default)
+        {
+            _value = value;
+            ValueChanged += _ => { };
+        }
+
+        public T Value
+        {
+            get => _value;
+            set
+            {
+                _value = value;
+                ValueChanged.Invoke(_value);
+            }
+        }
+
+        public event Action<T> OnValueChanged
+        {
+            add
+            {
+                ValueChanged += value;
+                ValueChanged.Invoke(_value);
+            }
+            remove
+            {
+                ValueChanged -= value;
+            }
+        }
+
+        private event Action<T> ValueChanged;
+    }
+}
